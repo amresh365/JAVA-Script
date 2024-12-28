50ccf609abb48d2a5075f9aafd4c32baedd68683
Amresh Gupta <114059788+amresh365@users.noreply.github.com>
2024-12-28 12:24:08 +0530

    Update callApplyBind.js

--- NJ/callApplyBind.js
+++ NJ/callApplyBind.js
@@ -1,3 +1,30 @@
+In JavaScript, call, apply, and bind are methods that allow you to control the this context in functions and enhance 
+function invocation. Here’s a quick overview of each:
+
+1. call
+Usage: function.call(thisArg, arg1, arg2, ...)
+Description: Invokes a function with a specified this value and individual arguments. You use call when you want to invoke 
+a function immediately and set the context of this.
+
+ 2. apply
+Usage: function.apply(thisArg, [argsArray])
+Description: Similar to call, but the arguments are provided as an array (or an array-like object). This is useful when you
+want to pass a variable number of arguments as an array. 
+
+3. bind
+Usage: function.bind(thisArg, arg1, arg2, ...)
+Description: Creates a new function that, when called, has its this keyword set to a provided value, with a given sequence
+of arguments preceding any provided when the new function is called. It does not call the function immediately; instead,
+it returns a new function that can be called later.
+
+
+call is for invoking a function immediately with a specific this context and individual arguments.
+apply is for invoking a function immediately with a specific this context, but with arguments as an array.
+bind creates a new function with a fixed this.
+These methods are particularly useful in scenarios involving event handling, callbacks, and when working with methods that 
+can be used as constructors or in a different context than originally intended.
+
+  
 const name1 = {
   Fname: "Amresh",
   Lname: "gupta",
